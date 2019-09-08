c1d18e0ee28463ef7e8f661d08e336cbe8ba2420
storfang <38114156+storfang@users.noreply.github.com>
2019-09-08 15:14:01 +0200

    post_final

---
+++ run_post/xcelium.d/run.lnx86.18.09.d/files.ts
@@ -0,0 +1,4 @@
+1559902804 /cad/dk/umc180/SUS/verilog/UCLLIB_AGH.v
+1567870202 /home/student/moleszkowicz/PPCU_VLSI/PPCU_VLSI_2019_projekt_verilog/tb/mtm_Alu_tb.v
+1567870202 /home/student/moleszkowicz/PPCU_VLSI/PPCU_VLSI_2019_projekt_verilog/tb/mtm_Alu_test_top.v
+1567931906 /home/student/moleszkowicz/PPCU_VLSI/PPCU_VLSI_2019_projekt_verilog/pr/RESULTS_PR/mtm_Alu.noPower.v.gz
